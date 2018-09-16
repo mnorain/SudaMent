7b77b6a2f8e3e9a8e71c33a3ba1b152852b6b905
rania idris <rania_idris@outlook.com>
2018-09-15 19:00:07 -0700

    feat: Added user identifier helper functions

--- application.py
+++ application.py
@@ -127,6 +127,24 @@ def gconnect():
 
 # User Helper Functions
 
+def createUser(login_session):
+       newUser = User(name=login_session['username'], email=login_session[
+                      'email'], picture=login_session['picture'])
+       session.add(newUser)
+       session.commit
+       user = session.query(User).filter_by(email=login_session['email']).one()
+       return user.id
+def getUserInfo(user_id):
+       user = session.query(User).filter_by(id=user_id).one
+       return user
+
+def getUserID(email):
+       try:
+              user = session.query(User).filter_by(email=email).one()
+              return user.id
+       except:
+              return None
+
 @app.route('/')
 def showHome():
 	#return("this page to show home page")
